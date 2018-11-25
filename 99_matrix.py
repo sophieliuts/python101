2b147ad1f58e44d390c6c679f402edddd9f351df
sophieliuts <sophie.liuts@gmail.com>
2018-11-25 10:21:33 +0800

    my first homework

---
+++ 99_matrix.py
@@ -0,0 +1,28 @@
+a=1
+b=2
+c=3
+d=4
+e=5
+f=6
+g=7
+h=8
+i=9
+for x in range (1, 10):
+    print ('%s x %s = %s' % (a,x, a*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (b,x, b*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (c,x, c*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (d,x, d*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (e,x, e*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (f,x, f*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (g,x, g*x))
+for x in range (1, 10):
+    print ('%s x %s = %s' % (h,x, h*x)) 
+for x in range (1, 10):
+    print ('%s x %s = %s' % (i,x, i*x))
+ 
---
+++ hello.py
@@ -0,0 +1,10 @@
+print(5+30*20/10)
+
+name = 'Sophie'
+age =9
+Sophie = 'my'
+print(name,age*10)
+message='Sophie age is %s'
+print(message % age)
+message = 'Sophie name is %s'
+print (message % name)
